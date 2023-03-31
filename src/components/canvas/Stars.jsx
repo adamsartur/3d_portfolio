f5d3b4fa2b1f55dfc9a2091b309ffa408c4573c9
adamsartur <adamsartur@gmail.com>
2023-03-31 01:56:30 -0300

    fix stars

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -1,18 +1,16 @@
 import {useState, useRef, Suspense} from 'react'
 import { Canvas, useFrame } from '@react-three/fiber'
 import { Points, PointMaterial, Preload } from '@react-three/drei'
-
 import * as random from 'maath/random/dist/maath-random.esm'
 
 
-const Stars = () => {
+const Stars = (props) => {
   const ref = useRef();
-  
   const [sphere] = useState(() => random.inSphere(new Float32Array(5000), { radius: 1.2 }));
 
   useFrame((state, delta) => {
-    ref.current.rotation.x -= delta/10;
-    ref.current.rotation.y -= delta/15;
+    ref.current.rotation.x -= delta / 10;
+    ref.current.rotation.y -= delta / 15;
   })
 
   return (
@@ -32,11 +30,11 @@ const Stars = () => {
   )
 }
 
-const StarsCanvas = () => {
+const StarsCanvas = () => (
   <div className='w-full h-auto absolute inset-0 z-[-1]'>
     <Canvas
       camera={{
-        position: [0, 0, 1],
+        position: [0, 0, 1]
       }}
     >
       <Suspense fallback={null}>
@@ -46,6 +44,6 @@ const StarsCanvas = () => {
       <Preload all />
     </Canvas>
   </div>
-}
+  )
 
 export default StarsCanvas
